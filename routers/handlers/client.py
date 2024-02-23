934c2b688a52a78946d7d73a7a68d7d32b98a521
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-23 14:46:55 +0600

    added(databesa(sqlite3))

---
+++ routers/handlers/client.py
@@ -0,0 +1,39 @@
+from aiogram import types , Router
+from config import dp, bot
+from aiogram.filters import CommandStart, Command
+from database.bot_db import save_to_database, get_saved_data
+from aiogram.types import ParseMode
+router = Router(name=__name__)
+
+@router.message(CommandStart())
+async def start_command(message: types.Message):
+    await bot.send_message(message.from_user.id, f"Здраствуйте сэр {message.from_user.full_name}")
+
+@router.message(Command('help', prefix="!/"))
+async def handle_help(message: types.Message):
+    await bot.send_message(message.from_user.id, "Чем могу помочь")
+
+@router.message(Command('save', prefix="!/"))
+async def handle_save(message: types.Message):
+    content_type = message.content_type()
+    content_data = await message.content_data()
+
+    user_id = message.from_user.id
+    save_to_database(user_id, content_type, content_data)
+    await message.reply("Сохранено!")
+
+@router.message(Command("my_save", prefix="!/"))
+async def handle_my_save(message: types.Message):
+    user_id = message.from_user.id
+
+    saved_data = get_saved_data(user_id)
+
+    if not saved_data:
+        await message.reply("У вас нету сохраненных данных")
+        return
+
+    result_message = "Ваши сохраненные данные \n"
+    for content_type, content_list in saved_data:
+        result_message += f"{content_type.capitalize()}: ({content_list})\n"
+
+    await message.reply(result_message, parse_mode=ParseMode.MARKDOWN)
